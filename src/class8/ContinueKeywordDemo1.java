3ee5d67935a9f0cd80b9e11ea12c192077727588
dnguyen0127 <dnguyen0127@gmail.com>
2023-01-17 17:36:29 -0500

    first commit

---
+++ src/class8/ContinueKeywordDemo1.java
@@ -0,0 +1,15 @@
+package class8;
+
+public class ContinueKeywordDemo1 {
+    public static void main(String[] args) {
+
+        for(int i=0; i<10; i++){
+
+            if(i==3||i==6||i==9){
+                continue;   //skip
+            }
+
+            System.out.println(i);
+        }
+    }
+}
