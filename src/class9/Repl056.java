8e7d7ad1dee5cb0a6eeea6df930866d540686f06
dnguyen0127 <dnguyen0127@gmail.com>
2023-01-17 18:47:28 -0500

    2nd commit

---
+++ src/class9/Repl056.java
@@ -0,0 +1,11 @@
+package class9;
+
+public class Repl056 {
+    public static void main(String[] args) {
+        for(int i=2;i<=14;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
