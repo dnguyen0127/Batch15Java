7473605af99fc6e4cca8ffa6f360616742fd4120
dnguyen0127 <dnguyen0127@gmail.com>
2023-01-17 22:33:46 -0500

    Class 10 Commit

---
+++ src/class10/D2Arrays.java
@@ -0,0 +1,27 @@
+package class10;
+
+import java.util.Arrays;
+
+public class D2Arrays {
+    public static void main(String[] args) {
+
+        //a 2d array which can hold 3 one d arrays
+        //each of size 3
+
+        String[][] amtrak=new String [3][3];
+        String[] cabin0={"Paulina","Hoa","Hardy"};
+        String[] cabin1={"Jared","Garbs","Dat"};
+        String[] cabin2={"Frank","John","Lin"};
+
+            amtrak[0]=cabin0;
+            amtrak[1]=cabin1;
+            amtrak[2]=cabin2;
+
+            //in the first [] we specify the address of 1 D Array
+        System.out.println(amtrak[1][2]);
+
+        System.out.println(Arrays.toString(amtrak[0]));
+
+
+    }
+}
