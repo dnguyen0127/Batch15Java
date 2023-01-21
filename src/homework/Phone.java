43c242affdec86b012197b20394364e5d9ef9943
dnguyen0127 <dnguyen0127@gmail.com>
2023-01-21 17:52:11 -0500

    1/21 commit

---
+++ src/homework/Phone.java
@@ -0,0 +1,16 @@
+package homework;
+
+public class Phone {
+        String  name;
+        String  color;
+        boolean smartphone;
+
+        void ring() {
+                System.out.println("ring ring");
+        }
+
+        void vibrate(){
+                System.out.println("vibrate vibrate");
+        }
+
+}
