d43e4c363d922fbe12c1b676a6e6389591231bb8
dnguyen0127 <dnguyen0127@gmail.com>
2023-01-22 17:11:50 -0500

    class 12 commit

---
+++ src/class12/StringDemo11.java
@@ -0,0 +1,12 @@
+package class12;
+
+public class StringDemo11 {
+    public static void main(String[] args) {
+
+        String str="Send it to support channel. More java";
+        String newStr=str.substring(5);
+        System.out.println(newStr);
+        //we can also specify the starting point and the ending point
+        System.out.println(str.substring(0,27));
+    }
+}
---
+++ src/class12/StringDemo9.java
@@ -0,0 +1,17 @@
+package class12;
+
+public class StringDemo9 {
+    public static void main(String[] args) {
+
+        String  str="jdnfkfd 3498234 ^*%*,asdfasdfsdf";
+        int counter=0;
+        for (int i=0; i<str.length(); i++){
+
+            if(Character.isAlphabetic(str.charAt(i))){
+                counter++;
+            }
+
+        }
+        System.out.println("Total isAlphabetic in Str "+counter);
+    }
+}
