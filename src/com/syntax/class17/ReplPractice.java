1cfe5dbf847213190bc22d98dbbd1f5a798255b1
dnguyen0127 <dnguyen0127@gmail.com>
2023-02-01 00:09:02 -0500

    1/31 commit

---
+++ src/com/syntax/class17/ReplPractice.java
@@ -0,0 +1,15 @@
+package com.syntax.class17;
+
+public class ReplPractice {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int time(int hours, int minutes){
+
+        System.out.println(hours+":"+minutes);
+
+
+}
---
+++ src/com/syntax/class17/Task3.java
@@ -0,0 +1,30 @@
+package com.syntax.class17;
+
+public class Task3 {
+    public static void main(String[] args) {
+        /*
+        Create a method that will accept a String as a parameter and return
+        a new String that consist only of vowels.
+        Method should be available inside the class only where it was
+        declared and executed by calling its name.
+
+
+        return type => String
+        name =>getOnlyVowels
+        parameters=> String inputStr
+
+
+         */
+
+        System.out.println(getOnlyVowels("Java"));
+
+
+
+        }
+
+    private static String  getOnlyVowels(String inputStr){
+        //AEIOUY aeiouy
+        //Asghar => Aa
+        return  inputStr.replaceAll("[^AEIOUYaeiouy]","");
+    }
+}
